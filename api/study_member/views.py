695c924b1db7b4dd18874189000c49036249b820
taxijjang <gw9122@naver.com>
2020-07-25 15:59:53 +0900

    study_memeber 오류수정

--- api/study_member/views.py
+++ api/study_member/views.py
@@ -5,24 +5,26 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+
 class StudyMemberList(APIView):
     def get(self, request):
-        study_member = StudyMember.object.all()
+        study_member = StudyMember.objects.all()
         serializer = StudyMemberSerializer(study_member, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = StudyMemberSerializer(data = request.data)
+        serializer = StudyMemberSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
 
 class StudyMemeberDetail(APIView):
     def get_object(self, pk):
-        return get_object_or_404(StudyMember,pk = pk)
+        return get_object_or_404(StudyMember, pk=pk)
 
-    def get(self,request, pk):
+    def get(self, request, pk):
         study_member = self.get_object(pk)
         serializer = StudyMemberSerializer(study_member)
         return Response(serializer.data, status=status.HTTP_200_OK)
