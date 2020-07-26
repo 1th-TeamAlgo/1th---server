d003b7a98e67323691a7be41b15d9244cf28ea12
MunSeongKim <chyin370@naver.com>
2020-07-26 16:25:48 +0900

    deploy

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,10 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'schwisestudy',
         'HOST': 'wisedb.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
-        'PORT': 3306,
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
+        'PORT': 3306
+        # 'OPTIONS': {
+            # 'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        # }
     }
 }
 
