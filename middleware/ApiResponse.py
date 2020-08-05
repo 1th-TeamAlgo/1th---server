c56b0244a3198cf02fbba833e8269c17096a67bc
taxijjang <gw9122@naver.com>
2020-08-05 15:40:20 +0900

    feature/#44/global_error_handler

---
+++ middleware/ApiResponse.py
@@ -0,0 +1,58 @@
+import re
+from rest_framework.status import is_client_error
+
+
+class ResponseFormattingMiddleware:
+    METHOD = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.API_URLS = [
+            re.compile(r'^(.*)/api/v1'),
+            re.compile(r'^api/v1'),
+        ]
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
+
+    def process_response(self, request, response):
+        print(request.path_info)
+        path = request.path_info
+        valid_urls = (url.match(path) for url in self.API_URLS)
+
+        if request.method in self.METHOD and any(valid_urls):
+            response_format = {
+                'code': "200",
+                'status': "OK",
+                'message': {}
+
+            }
+
+            if hasattr(response, 'data') and getattr(response, 'data') is not None:
+                data = response.data
+                try:
+                    response_format['message'] = data.pop('message')
+                except (KeyError, TypeError):
+                    response_format.update({
+                        'code': response.status_code,
+                        'status': response.status_text,
+                        'message': data,
+
+                    })
+                finally:
+                    if is_client_error(response.status_code):
+                        response_format['code'] = response.status_code
+                        response_format['status'] = response.status_text
+                        response_format['message'] = data
+                    else:
+                        response_format['message'] = data
+
+                    response.data = response_format
+                    response.content = response.render().rendered_content
+            else:
+                response.data = response_format
+
+        return response
