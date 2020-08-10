642cfcefa70003121c56a15bc7d31969b9d16d15
taxijjang <gw9122@naver.com>
2020-08-10 14:35:32 +0900

    feature/#69/study-member add [age,category]

--- api/study_member/serializers.py
+++ api/study_member/serializers.py
@@ -6,13 +6,12 @@ class StudyMemberSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='user.user_id')
     name = serializers.ReadOnlyField(source='user.name')
     email = serializers.ReadOnlyField(source='user.email')
+    age = serializers.ReadOnlyField(source='user.age')
     birthday = serializers.ReadOnlyField(source='user.birthday')
     cellphone = serializers.ReadOnlyField(source='user.cellphone')
     gender = serializers.ReadOnlyField(source='user.gender')
+    category = serializers.ReadOnlyField(source='user.categories')
 
     class Meta:
         model = StudyMember
-        fields = ['is_manager', 'id', 'name', 'email', 'birthday', 'cellphone', 'gender', ]
-
-
-
+        fields = ['is_manager', 'id', 'name', 'age', 'email', 'birthday', 'cellphone', 'gender', 'category', ]
