6d88ffe629217e777c5beed67a39a70001b5706a
taxijjang <gw9122@naver.com>
2020-08-11 19:27:54 +0900

    feat(#72):안드로이드 요구사항에 따른 schedule ã 수정

--- api/schedule/serializers.py
+++ api/schedule/serializers.py
@@ -3,11 +3,14 @@ from .models import Schedule
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    title = serializers.ReadOnlyField(source='study.title')
+
     class Meta:
         model = Schedule
-        fields = ['schedule_id', 'study', 'datetime', 'place', 'address', 'title', 'description']
+        fields = ['schedule_id', 'study', 'title', 'datetime', 'place', 'address', 'title', 'description']
+
 
 class ScheduleDeleteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Schedule
-        fields = ['study', 'title',]
+        fields = ['study', 'title', ]
