0734d2f836cc340d63cd11914fa74043347f7eb4
taxijjang <gw9122@naver.com>
2020-08-15 09:20:01 +0900

    reface jwt

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,15 +1,31 @@
 import requests
 import jwt
+
+from drf_yasg.utils import swagger_auto_schema
 from config.settings.secret import SECRET_KEY
 
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import json
 
 class KakaoAccount(APIView):
     renderer_classes = [JSONRenderer]
 
+    dummy_jwt = {
+        "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ ... 3XirwWnsXXrq0c3PMhRLEz5mFqbz2S2r_QxEM2Q"
+    }
+
+    @swagger_auto_schema(
+        responses={200: json.dumps(dummy_jwt)},
+        tags=['jwt'],
+        operation_description=
+        """
+        Kakao access token으로 jwt 발급
+
+        """,
+    )
     def get(self, request, format=None):
         access_token = request.META['HTTP_KAKAO_ACCESS_TOKEN']
         kakao_account = self.get_kakao_account(access_token=access_token)
--- api/signup/views.py
+++ api/signup/views.py
@@ -9,22 +9,22 @@ from rest_framework.views import APIView
 import json
 
 class UserSignUp(APIView):
-    @swagger_auto_schema(
-        request_body=UserSignUpSerializer,
-        responses={201: UserSignUpSerializer()},
-        tags=['users'],
-        operation_description=
-        """      
-        회원 생성 API
-        
-        ---
-            요청사양
-                - name : 이름
-                - age : 나이
-                - cellphone : 휴대폰 번호
-                - description : 자기소개
-        """,
-    )
+    # @swagger_auto_schema(
+    #     request_body=UserSignUpSerializer,
+    #     responses={201: UserSignUpSerializer()},
+    #     tags=['users'],
+    #     operation_description=
+    #     """
+    #     회원 생성 API
+    #
+    #     ---
+    #         요청사양
+    #             - name : 이름
+    #             - age : 나이
+    #             - cellphone : 휴대폰 번호
+    #             - description : 자기소개
+    #     """,
+    # )
     def post(self, request):
         serializer = UserSignUpSerializer(data=request.data)
         if serializer.is_valid():
