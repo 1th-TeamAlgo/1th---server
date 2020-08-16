9c1e8197a50d58ac88dd20cf24cada8a4a219025
taxijjang <gw9122@naver.com>
2020-08-16 18:06:46 +0900

    detail

--- api/user/views.py
+++ api/user/views.py
@@ -29,7 +29,7 @@ class UserList(APIView):
         user_payload = self.jwt_get_payload(request)
         pk = user_payload['user_id']
         user = self.get_object(pk)
-        serializer = UserSerializer(user)
+        serializer = UserDetailSerializer(user)
         return Response(serializer.data)
 
 
