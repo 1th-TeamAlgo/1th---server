07713d10deb846e857a3def3b7ba7bcea9c1805f
unknown <gw9122@naver.com>
2020-09-04 23:18:19 +0900

    m

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,33 +11,33 @@ ALLOWED_HOSTS = ['*']
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'wisedb',
-#         'USER': 'root',
-#         'PASSWORD': 'schwisestudy',
-#         'HOST': 'wisedb.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
-#         'PORT': 3306,
-#         'OPTIONS': {
-#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wisedb',
-        'USER': 'b5d1e551889167',
-        'PASSWORD': 'b5d1e551889167',
-        'HOST': 'us-cdbr-east-02.cleardb.com',
+        'USER': 'root',
+        'PASSWORD': 'schwisestudy',
+        'HOST': 'wisedb.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
         'PORT': 3306,
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
         }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wisedb',
+#         'USER': 'b5d1e551889167',
+#         'PASSWORD': 'b5d1e551889167',
+#         'HOST': 'us-cdbr-east-02.cleardb.com',
+#         'PORT': 3306,
+#         'OPTIONS': {
+#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+#         }
+#     }
+# }
 
 
 WSGI_APPLICATION = 'config.wsgi.production.application'
