c25ce51a8b0e46e1fa4c6585ab4a641ad2cad881
unknown <gw9122@naver.com>
2020-09-04 23:06:08 +0900

    dbchange

--- api/user/models.py
+++ api/user/models.py
@@ -14,6 +14,6 @@ class User(models.Model):
     categories = models.CharField(max_length=1024, null=True)
     kakao_profile_img = models.CharField(max_length=1024,null=True)
     s3_profile_img = models.FileField()
-    img_flag = models.BooleanField(default=True)
+    img_flag = models.BooleanField(default=False)
     def __str__(self):
         return self.name
