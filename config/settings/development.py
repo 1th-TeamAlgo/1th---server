0202d83c4c4c16f067a5942aaf458965139860a6
taxijjang <gw9122@naver.com>
2020-09-14 15:58:34 +0900

    db change rds

--- config/settings/development.py
+++ config/settings/development.py
@@ -25,4 +25,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wisestudy',
+#         'USER': 'root',
+#         'PASSWORD': 'schwisestudy',
+#         'HOST': 'wisestudy.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': 3306,
+#         'OPTIONS': {
+#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+#         }
+#     }
+# }
 WSGI_APPLICATION = 'config.wsgi.production.application'
--- config/settings/stage.py
+++ config/settings/stage.py
@@ -15,10 +15,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'heroku_91058f0babf3969',
-        'USER': 'b07cec7fd5b708',
-        'PASSWORD': '527ea9e5',
-        'HOST': 'us-cdbr-east-02.cleardb.com',
+        'NAME': 'wisestudy',
+        'USER': 'root',
+        'PASSWORD': 'schwisestudy',
+        'HOST': 'wisestudy.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
         'PORT': 3306,
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
