d915d04b285dac165008b6df4ea46d6ea2678c60
taxijjang <gw9122@naver.com>
2020-09-26 18:12:42 +0900

    test

--- api/study_member/views/study_member_confirm.py
+++ api/study_member/views/study_member_confirm.py
@@ -65,7 +65,7 @@ class StudyMemberConfirm(APIView):
             # study_members_serializer = MemberOfStudySerializer(study_members)
             # return Response(study_members_serializer.data)
 
-            return Response(data=[study_member_data])
+            return Response(data=[study_member_data,])
         else:
             return Response(data=['관리자가 아닙니다'])
 
