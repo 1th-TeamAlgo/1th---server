bdef19f64fa7e89871b483380e5ed4ad6fc5a617
taxijjang <gw9122@naver.com>
2020-09-28 16:41:57 +0900

    test

--- api/study_member/views/study_member_confirm.py
+++ api/study_member/views/study_member_confirm.py
@@ -12,6 +12,7 @@ from django.core.cache import cache
 
 from lib.user_data import jwt_get_payload
 
+import json
 
 class StudyMemberConfirm(APIView):
     @swagger_auto_schema(
@@ -32,16 +33,18 @@ class StudyMemberConfirm(APIView):
 
         if self.user_is_manager(user_id=user_payload['user_id'], studies_id=study_id):
             str_study_id = self.str_study_id(study_id)
-            user_id = request.POST.get('user_id')
+
+            data = json.loads(request.body)
+            user_id = data['user_id']
+
             print(user_id)
             self.apply_member_delete_redis(str_study_id, user_id)
 
             if len(StudyMember.objects.filter(study_id=study_id, user_id=user_id)) > 0:
                 return Response(data=["이미 들어있다"])
 
-            print(user_id)
             user = get_object_or_404(User, pk=user_id)
-
+            print(f"+++++++ {user} +++++++")
             study = get_object_or_404(Study, pk=study_id)
             study.study_members_count += 1
             study.save()
@@ -65,14 +68,15 @@ class StudyMemberConfirm(APIView):
     )
     ## study 가입 신청 멤버 반려
     def delete(self, request, *args, **kwargs):
+        print("delete 들어온다")
         user_payload = jwt_get_payload(request)
         study_id = self.kwargs['studies_id']
 
         if self.user_is_manager(user_id=user_payload['user_id'], studies_id=study_id):
             str_study_id = self.str_study_id(study_id)
 
-            user_id = request.POST.get('user_id')
-
+            data = json.loads(request.body)
+            user_id = data['user_id']
             # study_apply_dict = self.apply_member_delete_redis(str_study_id, user_id)
             # return Response(data=study_apply_dict)
             self.apply_member_delete_redis(str_study_id, user_id)
@@ -84,7 +88,6 @@ class StudyMemberConfirm(APIView):
     ## 가입 승인 또는 반려 된 인원 redis에서 제거
     def apply_member_delete_redis(self, study_id, user_id):
         study_apply_lists = cache.get(study_id)
-
         delete_index = 0
         for i, study_apply_list in enumerate(study_apply_lists):
             if hasattr(study_apply_list, 'user_id') is not None and study_apply_list['user_id'] == user_id:
@@ -101,9 +104,12 @@ class StudyMemberConfirm(APIView):
         return 'study:' + str(study_id)
 
     def user_is_manager(self, studies_id, user_id):
+        print(user_id)
         is_manager = get_object_or_404(StudyMember, study=studies_id, user=user_id)
         print(is_manager)
         if hasattr(is_manager, 'is_manager') is not None and getattr(is_manager, 'is_manager') is True:
+            print("매니저 ok")
             return True
         else:
+            print("매너지 X")
             return False
